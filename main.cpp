b4ad44948abcb68e665e32ee91cad655b7a9f775
Alexey Abramov <alexpoligon@mail.ru>
2019-12-24 20:09:23 +0300

    async out to files implemented

--- main.cpp
+++ main.cpp
@@ -13,8 +13,21 @@ int main(int, char *[]) {
     async::receive(h, "\n2\n3\n4\n5\n6\n{\na\n", 15);
     async::receive(h, "b\nc\nd\n}\n89\n", 11);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
 
+    std::thread t{
+        [] () {
+        std::size_t bulk1 = 3;
+        auto h3 = async::connect(bulk1);
+        async::receive(h3, "100\n200\n300\n400\n500\n600\n700\n800\n900\n1000\n1100\n1200\n1300\n", 1);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        async::disconnect(h3);
+    }};
+
+    if (t.joinable()) {
+        t.join();
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     async::disconnect(h);
     async::disconnect(h2);
 
